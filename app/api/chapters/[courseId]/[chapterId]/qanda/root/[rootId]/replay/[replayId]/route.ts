ec73736d5d08f92c658718add4318457ed7ca2bf
PhuocVinhLee <vinhb1910176@student.ctu.edu.vn>
2024-08-13 14:22:07 +0700

    i delete GET

--- app/api/chapters/[courseId]/[chapterId]/qanda/root/[rootId]/replay/[replayId]/route.ts
+++ app/api/chapters/[courseId]/[chapterId]/qanda/root/[rootId]/replay/[replayId]/route.ts
@@ -3,9 +3,8 @@ import { auth, currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
-import Courses from "@/lib/database/models/courses.model";
 import { connectToDatabase } from "@/lib/database/mongoose";
-import Chapters from "@/lib/database/models/chapters.model";
+
 import { getUserById } from "@/lib/actions/user.actions";
 import QandA from "@/lib/database/models/qanda.model";
 import { pusherServer } from "@/lib/pusher";
@@ -99,45 +98,45 @@ export async function POST(
   }
 }
 
-export async function GET(
-  req: Request,
-  { params }: { params: { rootId: string; replayId: string } }
-) {
-  try {
-    const { userId } = auth();
+// export async function GET(
+//   req: Request,
+//   { params }: { params: { rootId: string; replayId: string } }
+// ) {
+//   try {
+//     const { userId } = auth();
    
-    if (!userId) {
-      return new NextResponse("UnAuthention", {
-        status: 401,
-      });
-    }
-    const current_User = await currentUser();
-    const { rootId, replayId } = params;;
+//     if (!userId) {
+//       return new NextResponse("UnAuthention", {
+//         status: 401,
+//       });
+//     }
+//     const current_User = await currentUser();
+//     const { rootId, replayId } = params;;
 
-    console.log(current_User);
-    const isTeacher = current_User?.publicMetadata?.role === "teacher";
+//     console.log(current_User);
+//     const isTeacher = current_User?.publicMetadata?.role === "teacher";
 
-    await connectToDatabase();
-    const user = await getUserById(userId);
-    if (!user) {
-      return new NextResponse("User not found", { status: 401 });
-    }
-    const showMoreMessage = await QandA.find({
-      root: false,
-      rootId: rootId,
-    })
-      .sort({ createdAt: -1 })
-      .populate("userId", "username photo role") // Specify the fields you want to include from the Chapters model
-      .populate("userIdReplay", "username photo role")
-      .exec();
-    if (!showMoreMessage) {
-      return new NextResponse("ShowMoreMessage not found", { status: 401 });
-    }
+//     await connectToDatabase();
+//     const user = await getUserById(userId);
+//     if (!user) {
+//       return new NextResponse("User not found", { status: 401 });
+//     }
+//     const showMoreMessage = await QandA.find({
+//       root: false,
+//       rootId: rootId,
+//     })
+//       .sort({ createdAt: -1 })
+//       .populate("userId", "username photo role") // Specify the fields you want to include from the Chapters model
+//       .populate("userIdReplay", "username photo role")
+//       .exec();
+//     if (!showMoreMessage) {
+//       return new NextResponse("ShowMoreMessage not found", { status: 401 });
+//     }
 
-    //console.log(".sort({ createdAt: 1 })",showMoreMessage )
-    return NextResponse.json(showMoreMessage);
-  } catch (error) {
-    console.log("erorr in Update chapter", error);
-    return new NextResponse("Inter Error", { status: 500 });
-  }
-}
+//     //console.log(".sort({ createdAt: 1 })",showMoreMessage )
+//     return NextResponse.json(showMoreMessage);
+//   } catch (error) {
+//     console.log("erorr in Update chapter", error);
+//     return new NextResponse("Inter Error", { status: 500 });
+//   }
+// }
